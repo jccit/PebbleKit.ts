c7f994c5597c623cb2f5545da67a3f4e4ed62796
Joe Citrine <josef@citrine.dev>
2025-08-01 15:40:37 +0100

    Add commonjs support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/ts/index.ts",
@@ -12,6 +13,7 @@ export default {
   plugins: [
     typescript(),
     nodeResolve(),
+    commonjs(),
     babel({ babelHelpers: "inline", extensions: [".ts", ".js"] }),
   ],
 };
