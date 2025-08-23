85d7d9b71d892a69c5a68d265f30f37db59e06c5
Joe Citrine <josef@citrine.dev>
2025-08-23 23:22:20 +0100

    Generate basic docs

--- src/tslib/index.ts
+++ src/tslib/index.ts
@@ -1,7 +1,17 @@
+/**
+ * PebbleTS public API
+ * @packageDocumentation
+ * @module PebbleTS
+ */
 import { sendAppMessage } from "./appMessage";
 import { getTimelineToken } from "./timeline";
 import { fetchString, fetchJSON, fetchBinary } from "./fetch";
 
+export { sendAppMessage } from "./appMessage";
+export { getTimelineToken } from "./timeline";
+export { fetchString, fetchJSON, fetchBinary } from "./fetch";
+
+/** @internal */
 export default {
   sendAppMessage,
   getTimelineToken,
