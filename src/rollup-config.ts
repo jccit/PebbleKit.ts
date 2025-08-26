4898ace37f3b6f5c9cd37df14009402afd23c184
Joe Citrine <josef@citrine.dev>
2025-08-26 23:56:47 +0100

    Add json plugin

--- src/rollup-config.ts
+++ src/rollup-config.ts
@@ -3,6 +3,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
 import path from "path";
 import { pktsDir } from "./util";
 
@@ -17,6 +18,7 @@ export default {
     typescript(),
     nodeResolve(),
     commonjs(),
+    json(),
     babel({
       babelHelpers: "inline",
       extensions: [".ts", ".js"],
