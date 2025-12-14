3cef28e521420bf4ad9a7d2d5cc0a3323ca46ccb
Joe Citrine <josef@citrine.dev>
2025-12-14 22:27:43 +0000

    Core devices local timeline api

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,16 @@
 import { sendAppMessage } from "./appMessage";
-import { getTimelineToken } from "./timeline";
+import {
+  getTimelineToken,
+  insertTimelinePin,
+  deleteTimelinePin,
+} from "./timeline";
 import { fetchString, fetchJSON, fetchBinary } from "./fetch";
 
 export default {
   sendAppMessage,
   getTimelineToken,
+  insertTimelinePin,
+  deleteTimelinePin,
   fetchString,
   fetchJSON,
   fetchBinary,
--- lib/timeline.ts
+++ lib/timeline.ts
@@ -7,3 +7,29 @@ export function getTimelineToken(): Promise<string> {
     Pebble.getTimelineToken(resolve, reject);
   });
 }
+
+/**
+ * Adds a timeline pin to the user's timeline.
+ * Only works on the Core Devices app. This function will throw when not available.
+ * @param pin - TimelinePin object or string
+ * @throws Error if insertTimelinePin is not available
+ */
+export function insertTimelinePin(pin: TimelinePin | string): void {
+  if (!Pebble.insertTimelinePin) {
+    throw new Error("insertTimelinePin is not available");
+  }
+  Pebble.insertTimelinePin(pin);
+}
+
+/**
+ * Deletes a pin from the user's timeline by id.
+ * Only works on the Core Devices app. This function will throw when not available.
+ * @param id - The ID of the timeline pin to remove
+ * @throws Error if removeTimelinePin is not available
+ */
+export function deleteTimelinePin(id: string): void {
+  if (!Pebble.deleteTimelinePin) {
+    throw new Error("deleteTimelinePin is not available");
+  }
+  Pebble.deleteTimelinePin(id);
+}
